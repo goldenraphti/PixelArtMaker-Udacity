9fda1817097b67e24b1337d7c08f3d12752607d0
goldenraphti <goldenraphti@users.noreply.github.com>
2018-01-16 21:39:43 +0100

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,10 +1,65 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+    
 
-function makeGrid() {
 
-// Your code goes here!
 
+function makeGrid() {   // Select size input
+    
+    const height = $("#input_height").val();
+    const width = $("#input_width").val();
+
+
+    for (let row = 0 ; row < height ; row++) {  
+        $("#pixel_canvas").append("<tr></tr>");
+    };
+    for (let column = 0 ; column < width ; column++) {  
+        $("tr").append("<td></td>");
+    };
+
+    
+};
+
+function eraseGrid() {
+    $("#pixel_canvas").empty();
 }
+
+
+
+
+
+function paintColor() {    // Select color input
+    
+    $("#pixel_canvas").on("click", "td" , function(){
+        let colorUser = $("#colorPicker").val();
+        $(this).css("background", colorUser);
+    })
+};
+
+    
+$(document).ready(function(){ // calls for functions
+    
+    $("#sizePicker").submit(function(evt){ // call for function to create grid
+        
+        evt.preventDefault();
+        eraseGrid();
+        makeGrid();
+
+    });
+
+    $("#erase-grid-button").click(function(evt){
+        evt.preventDefault();
+        eraseGrid();
+    });
+    
+    paintColor();
+
+});
+
+// TODO: explain your code
+
+// TODO: make a nice html-css new website for it
+
+// TODO: implement new buttons panel (minimized/maximize with tool icon)
+// TODO: background color button
+// TODO: export drawing as .png on open-source plateform (frama)
+// TODO: import a nice photo adjusted in the background (with low opacity)
