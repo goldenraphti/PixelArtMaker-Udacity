b1c203d3b5c7a281599342d523bd4aab023c9e8a
goldenraphti <goldenraphti@users.noreply.github.com>
2018-01-18 16:03:56 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Pixel Art Maker Project
 
 Project work for Udacity Front End Development course
-
-https://goldenraphti.github.io/PixelArtMaker-Udacity/
--- designs.js
+++ designs.js
@@ -1,13 +1,13 @@
-
+"use strict"; // Use javascript in strict mode in global scope
     
 
 
-
-function makeGrid() {   // Select size input
+function makeGrid() {   // Select and create grid size
     
     const height = $("#input_height").val();
     const width = $("#input_width").val();
 
+    let colorGridBackground = $("#colorBackground").val();
 
     for (let row = 0 ; row < height ; row++) {  
         $("#pixel_canvas").append("<tr></tr>");
@@ -15,51 +15,69 @@ function makeGrid() {   // Select size input
     for (let column = 0 ; column < width ; column++) {  
         $("tr").append("<td></td>");
     };
-
+    
+    $("td").css("background", colorGridBackground); // creates the css background color decided before submit grid creation
     
 };
 
-function eraseGrid() {
+function resetGrid() {  // Reset grid to initial color
+    let colorGridBackground = $("#colorBackground").val();
+     $("td").css("background", colorGridBackground);
+};
+
+function deleteGrid() {  // Erase eventual existing grid
     $("#pixel_canvas").empty();
-}
-
-
-
+};
 
 
 function paintColor() {    // Select color input
-    
-    $("#pixel_canvas").on("click", "td" , function(){
-        let colorUser = $("#colorPicker").val();
-        $(this).css("background", colorUser);
-    })
+
+    let mouseDownStatus = false;    // check if mouse is pressed so draw only when pressed
+    $(document).mousedown(function() {
+            mouseDownStatus = true;
+        }).mouseup(function() {
+            mouseDownStatus = false;
+    });
+                   
+
+    $("#pixel_canvas").on("mousemove", "td" , function(){
+        if (mouseDownStatus) {
+            let colorUser = $("#colorPicker").val();
+            $(this).css("background", colorUser);
+        };
+    });
+
 };
+
+
 
     
 $(document).ready(function(){ // calls for functions
     
+
+    
     $("#sizePicker").submit(function(evt){ // call for function to create grid
         
-        evt.preventDefault();
-        eraseGrid();
+        evt.preventDefault();   // prevent from reloading the page when click submit
+        deleteGrid();
         makeGrid();
 
     });
-
-    $("#erase-grid-button").click(function(evt){
+    
+    $("#eraser").click(function(evt){    // reset grid to initial colours
         evt.preventDefault();
-        eraseGrid();
+        resetGrid();
+    });
+
+    $("#delete-grid-button").click(function(evt){    // erase existing grid function
+        evt.preventDefault();
+        deleteGrid();
     });
     
-    paintColor();
+    paintColor();   //  call for painting function
+    
 
+    
+
+    
 });
-
-// TODO: explain your code
-
-// TODO: make a nice html-css new website for it
-
-// TODO: implement new buttons panel (minimized/maximize with tool icon)
-// TODO: background color button
-// TODO: export drawing as .png on open-source plateform (frama)
-// TODO: import a nice photo adjusted in the background (with low opacity)
